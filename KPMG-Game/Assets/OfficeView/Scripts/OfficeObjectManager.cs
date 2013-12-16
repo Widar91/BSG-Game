e37d9b8076f330e01b311fdabf28612e8c1eb774
Chris Froussios <darkmatter.tm@gmail.com>
2013-12-16 09:24:40 +0100

    Fixed wall material

---
+++ KPMG-Game/Assets/OfficeView/Scripts/OfficeObjectManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OfficeObjectManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject simpleTable = (GameObject) Instantiate(Resources.Load("OfficeView/TableSimple", typeof(GameObject)));
+		simpleTable.transform.position = new Vector3 (-4f,1f,9.7f);
+		simpleTable.renderer.material.color = Color.cyan;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
