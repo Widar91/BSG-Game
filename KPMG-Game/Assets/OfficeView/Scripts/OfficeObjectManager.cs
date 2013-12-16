48d28892edae2c02ef5f5ce56af36c830787403b
Chris Froussios <darkmatter.tm@gmail.com>
2013-12-16 10:43:28 +0100

    Dynamic loader

--- KPMG-Game/Assets/OfficeView/Scripts/OfficeObjectManager.cs
+++ KPMG-Game/Assets/OfficeView/Scripts/OfficeObjectManager.cs
@@ -1,17 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+public class Item
+{
+	public string resource;
+	public Vector3 position;
+	public Vector3 scale;
+	public Vector3 rotation;
+
+	public Item(string name, Vector3 position, float scale)
+	{
+		this.resource = name;
+		this.position = position;
+		this.scale = new Vector3 (scale,scale,scale);
+	}
+}
+
 public class OfficeObjectManager : MonoBehaviour {
+
+	Item[] items = 
+	{
+		new Item("TableSimple", new Vector3 (-3.97f,1.04f,-9.741f) , 5)
+	};
 
 	// Use this for initialization
 	void Start () {
-		GameObject simpleTable = (GameObject) Instantiate(Resources.Load("OfficeView/TableSimple", typeof(GameObject)));
-		simpleTable.transform.position = new Vector3 (-4f,1f,9.7f);
-		simpleTable.renderer.material.color = Color.cyan;
+		LoadItems (items);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void LoadItems(IEnumerable items)
+	{
+		foreach (Item item in items)
+		{
+			GameObject instance = (GameObject) Instantiate(Resources.Load(item.resource));
+			instance.transform.localScale = item.scale;
+			instance.transform.position = item.position;
+			//instance.renderer.material.color = Color.cyan;
+		}
 	}
 }
