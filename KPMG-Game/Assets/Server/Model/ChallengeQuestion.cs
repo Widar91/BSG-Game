f1bffc298cb3576d6bdce9c7632c1a5fdef0dcfd
Widar91 <eddy.bertoluzzo@gmail.com>
2013-12-17 23:54:49 +0100

    implemented connection to the database (locally)

---
+++ KPMG-Game/Assets/Server/Model/ChallengeQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class ChallengeQuestion {
+
+	private string question;
+	private List<string> answers;
+
+	public ChallengeQuestion (string question, List<string> answers) {
+		this.question = question;
+		this.answers = answers;
+	}
+
+	public string getQuestion() {
+		return this.question;
+	}
+
+	public List<string> getAnswers() {
+		return this.answers;
+	}
+	
+
+}
