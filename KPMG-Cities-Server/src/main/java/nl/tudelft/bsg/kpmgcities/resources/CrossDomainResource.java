d2df586bd3c7299f5707354595b4b8a5e4821da3
Widar91 <eddy.bertoluzzo@gmail.com>
2013-12-21 17:48:24 +0100

    Fix to unity web player connection problems

---
+++ KPMG-Cities-Server/src/main/java/nl/tudelft/bsg/kpmgcities/resources/CrossDomainResource.java
@@ -0,0 +1,21 @@
+package nl.tudelft.bsg.kpmgcities.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("")
+public class CrossDomainResource {
+
+	@GET
+	@Path("crossdomain.xml")
+	@Produces(MediaType.APPLICATION_XML)
+	public String getCrossDomain() {
+		return    "<?xml version=\"1.0\"?>"
+				+ "<cross-domain-policy>"
+				+ "<allow-access-from domain=\"*\"/>"
+				+ "</cross-domain-policy>";
+	}
+	
+}
