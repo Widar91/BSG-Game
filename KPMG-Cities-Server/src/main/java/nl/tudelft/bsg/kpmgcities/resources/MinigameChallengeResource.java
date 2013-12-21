d1abef5bc81e37588b09ace91ea2fa85ed65f49f
Widar91 <eddy.bertoluzzo@gmail.com>
2013-12-21 17:48:24 +0100

    Fix to unity web player connection problems

--- KPMG-Cities-Server/src/main/java/nl/tudelft/bsg/kpmgcities/resources/MinigameChallengeResource.java
+++ KPMG-Cities-Server/src/main/java/nl/tudelft/bsg/kpmgcities/resources/MinigameChallengeResource.java
@@ -1,13 +1,10 @@
 package nl.tudelft.bsg.kpmgcities.resources;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import nl.tudelft.bsg.kpmgcities.model.minigames.multiplayerchallenge.ChallengeQuestion;
 
 @Path("minigamechallenge")
 public class MinigameChallengeResource {
@@ -24,8 +21,7 @@ public class MinigameChallengeResource {
 	    }
 	
 	
-		@GET
-	    @Produces(MediaType.APPLICATION_JSON)
+		@POST
 	    public int getMinigameChallenge() {
 				
 				return 1;
