8fdacd2e6faa1c78a791c10d1e4944d5044aa239
Widar91 <eddy.bertoluzzo@gmail.com>
2014-01-09 12:01:09 +0100

    added login + building height

--- KPMG-Cities-Server/src/main/java/nl/tudelft/bsg/kpmgcities/resources/PlayerResource.java
+++ KPMG-Cities-Server/src/main/java/nl/tudelft/bsg/kpmgcities/resources/PlayerResource.java
@@ -18,4 +18,5 @@ public class PlayerResource {
     public Player getPlayer(@PathParam("username") String username) {
         return DBConnector.getInstance().getPlayer(username, "");
     }
+    
 }
---
+++ KPMG-Game/Assets/Login/Scripts/login_GUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class login_GUI : MonoBehaviour {
+
+	public GUISkin loginskin;
+	public static string uname = "";
+	public static string upass = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.BeginGroup (new Rect (Screen.width*0.375f,Screen.height*0.35f,Screen.width*0.5f,Screen.height*0.8f), "");
+
+		GUI.Box(new Rect(0,0,Screen.width*0.25f,Screen.height*0.3f),"");
+		GUI.Label (new Rect (20, 40, 200, 20), "Username : ");
+		uname = GUI.TextField (new Rect (90, 40, 200, 22), uname, 25);
+		GUI.Label (new Rect (20, 70, 200, 20), "Password : ");
+		upass = GUI.PasswordField (new Rect (90, 70, 200, 22), upass, "*"[0], 25);
+
+		if (GUI.Button (new Rect (130, 120, 70, 23), "Login") || Input.GetKeyDown (KeyCode.Return)) {
+			//Do Something
+			print("Logging in");
+				}
+
+		GUI.EndGroup ();
+		}
+}
